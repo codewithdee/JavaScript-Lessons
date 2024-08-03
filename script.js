2dcd70a0b3e1a3427325b361a1b2c18331e21d63
Dennis <dennisadjetey9@gmail.com>
2024-08-03 21:20:09 +0000

    Lesson_11 - 20

---
+++ script.js
@@ -0,0 +1,92 @@
+// DIVIDING NUMBERS
+var quotient = 80 / 5;
+console.log(quotient);
+
+
+//INCREMENTING NUMBERS
+var myVar = 45;
+myVar++
+console.log(myVar);
+
+
+//DECREMENTING NUMBERS
+var ourVar = 11;
+ourVar--;
+console.log(ourVar);
+
+
+//DECIMAL NUMBERS
+var ourDecimal = 5.5;
+var myDecimal = 0.009;
+console.log(myDecimal);
+console.log(ourDecimal);
+
+
+//MULTIPLY DECIMALS
+var product = 0.24 * 0.009;
+console.log(product);
+
+
+//DIVIDE DECIMALS
+var quotient = 4.4 / 2;
+console.log(quotient);
+
+
+//FINDIND A REMAINDER
+var remainder = 39 % 5;
+console.log(remainder)
+
+
+//COMPOUND ASSIGNMENT WITH AUGMENTED ADDITION
+var a = 13;
+var b = 17;
+var c = 12;
+
+a += 5;
+b += 3;
+c += 2;
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+//COMPOUND ASSIGNMENT WITH AUGMENTED SUBTRACTION
+var a = 13;
+var b = 17;
+var c = 12;
+
+a -= 5;
+b -= 3;
+c -= 2;
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+//COMPOUND ASSIGNMENT WITH AUGMENTED MULTIPLICATION
+var x = 0.2;
+var y = 4.5;
+var z = 6;
+
+x *= 5;
+y *= 3;
+z *= 2;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+//COMPOUND ASSIGNMENT WITH AUGMENTED DIVISION
+var a = 56;
+var b = 99;
+var c = 108;
+
+a /= 7;
+b /= 11;
+c /= 4;
+console.log(a);
+console.log(b);
+console.log(c);
